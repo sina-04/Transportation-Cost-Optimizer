1839418bfd0a4ff135969de48439271827d4a425
Sina Rezaei <152013952+sina-04@users.noreply.github.com>
2025-12-03 08:35:03 +0330

    Add new methods to MethodType enumeration

--- method_type.py
+++ method_type.py
@@ -1,8 +1,10 @@
 from enum import IntEnum
 
-
 # values of the possible methods to get an initial solution
 class MethodType(IntEnum):
     NORTH_WEST_METHOD = 1
     VOGEL_METHOD = 2
     RUSSELL_METHOD = 3
+    LEAST_COST_CELL_METHOD = 4
+    LEAST_COST_ROW_METHOD = 5
+    LEAST_COST_COLUMN_METHOD = 6
